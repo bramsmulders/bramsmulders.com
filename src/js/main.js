bbabdad4d4c5931ec07653230158488db20633cd
Bram Smulders <bram@bramsmulders.com>
2013-12-01 21:41:08 +0100

    no jquery

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,83 @@
     $LINKS
 \*----------------------------------------------------------------------------*/
 
+var scroller = {
+    currentYPosition: function() {
+        if (self.pageYOffset) {
+            return self.pageYOffset;
+        } else {
+            return 0;
+        }
+    },
+
+    elmYPosition: function(elementID) {
+        var elm = document.querySelector(elementID);
+        var y = elm.offsetTop;
+        while (elm.offsetParent && elm.offsetParent != document.body) {
+            elm = elm.offsetParent;
+            y += elm.offsetTop;
+        }
+        return y;
+    },
+
+    smoothScroll: function(elementID) {
+        var start = this.currentYPosition();
+        var stop = this.elmYPosition(elementID);
+        var distance = (stop > start)
+            ? stop - start
+            : start - stop
+        ;
+        if (distance < 100) {
+            scrollTo(0, stop);
+            return;
+        }
+        var speed = 7;
+        if (speed >= 20) {
+            speed = 20;
+        }
+        var step = Math.round(distance / 25);
+        var leap = (stop > start)
+            ? start + step
+            : start - step
+        ;
+        var timer = 0;
+        if (stop > start) {
+            for ( var i=start; i<stop; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leap+")", timer * speed);
+                leap += step;
+                if (leap > stop){
+                    leap = stop;
+                }
+                timer++;
+            } return;
+        }
+        for ( var i=start; i>stop; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leap+")", timer * speed);
+            leap -= step;
+            if (leap < stop){
+                leap = stop;
+            }
+            timer++;
+        }
+    }
+};
+
+
+
+
 var links = {
-    root: null,
     init: function(){
-        var self = this;
-        self.root = $('html, body');
+        this.target();
+        this.anchor();
+    },
 
-        $('a[rel*=external]').on('click', function(){
-            $(this).attr('target', '_blank');
-        });
-
-        self.anchor();
+    target: function(){
+        var links = document.querySelectorAll('a[rel="external"]');
+        for(var key in links){
+            links[key].onclick = function(){
+                this.setAttribute('target', '_blank');
+            }
+        }
     },
 
     anchor: function(){
@@ -27,36 +93,27 @@ var links = {
         ;
 
         if(ancloc.length){
-            self.locate(ancloc);
+            scroller.smoothScroll(ancloc);
             if(event && typeof event.preventDefault() === 'function'){
                 event.preventDefault();
             }
         }
-
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-                var target = this.hash;
-                if (target.length) {
-                    if(event && typeof event.preventDefault() === 'function'){
-                        event.preventDefault();
+        var links = document.querySelectorAll('a[href*="#"]:not([href="#"])');
+        for(var key in links) {
+            links[key].onclick = function(){
+                if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+                    var target = this.hash;
+                    if (target.length) {
+                        if(event && typeof event.preventDefault() === 'function'){
+                            event.preventDefault();
+                        }
+                        setTimeout(function() {
+                            scroller.smoothScroll(target);
+                        }, 200);
                     }
-                    setTimeout(function() {
-                        self.locate(target);
-                    }, 200);
                 }
-            }
-        });
-
-        return false;
-    },
-
-    locate: function(ancloc){
-        var self = this;
-        self.root.animate({
-            scrollTop: $(ancloc).offset().top
-        }, 300, function () {
-            window.location.hash = ancloc;
-        });
+            };
+        }
         return false;
     }
 };
@@ -70,6 +127,6 @@ var links = {
     Fire ALL the things
 \*----------------------------------------------------------------------------*/
 
-$(document).ready(function(){
+window.onload = function(){
     links.init();
-});
+}
