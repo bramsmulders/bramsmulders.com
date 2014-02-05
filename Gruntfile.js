53e1fb6de657d9b51f020eda32967754794dde46
Bram Smulders <bram@bramsmulders.com>
2014-02-05 09:59:47 +0100

    performance boost

--- Gruntfile.js
+++ Gruntfile.js
@@ -272,15 +272,15 @@ module.exports = function (grunt) {
         htmlmin: {
             build: {
                 options: {
-                    // removeCommentsFromCDATA: true,
-                    // https://github.com/yeoman/grunt-usemin/issues/44
-                    // collapseWhitespace: true,
-                    // collapseBooleanAttributes: true,
-                    // removeAttributeQuotes: true,
-                    // removeRedundantAttributes: true,
-                    // useShortDoctype: true,
-                    // removeEmptyAttributes: true,
-                    // removeOptionalTags: true
+                    removeCommentsFromCDATA: true,
+                    //https://github.com/yeoman/grunt-usemin/issues/44
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeOptionalTags: true
                 },
                 files: [{
                     expand: true,
@@ -320,8 +320,8 @@ module.exports = function (grunt) {
                 'coffee',
                 'compass',
                 'autoprefixer',
-                'imagemin'/*,
-                'htmlmin'*/
+                'imagemin',
+                'htmlmin'
             ]
         },
         bower: {
