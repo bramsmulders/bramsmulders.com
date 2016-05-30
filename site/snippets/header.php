14b440a3cff39a3b071f9ff8014b2bdee4ba9187
Bram Smulders <bram@bramsmulders.com>
2016-05-30 18:53:41 +0200

    fix indentation

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,21 +2,19 @@
 <html lang="en">
 <head>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+    <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <meta name="description" content="<?php echo $site->description()->html() ?>">
+    <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/style.css') ?>
-
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+    <?php echo css('assets/css/style.css') ?>
 </head>
 <body>
 
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
+    <header role="banner">
+        <?php snippet('menu') ?>
+    </header>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,16 +1,16 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+    <main role="main">
 
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
+        <div class="text">
+            <h1><?php echo $page->title()->html() ?></h1>
+            <?php echo $page->text()->kirbytext() ?>
+        </div>
 
-    <hr>
+        <hr>
 
-    <?php snippet('projects') ?>
+        <?php snippet('projects') ?>
 
-  </main>
+    </main>
 
 <?php snippet('footer') ?>
