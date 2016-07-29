b84512d83d822487afccacc14ae8dbd04cdc605e
Bram Smulders <bram@bramsmulders.com>
2016-07-29 17:01:48 +0200

    Fixed some styling

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,8 +2,19 @@
 
     <main role="main">
 
-        <h1><?php echo $page->title()->html() ?></h1>
-        <?php echo $page->text()->kirbytext() ?>
+
+        <div class="o-layer  t-zebra">
+
+            <div class="o-retain  o-retain--readable">
+
+                <h1><?php echo $page->title()->html() ?></h1>
+                <?php echo $page->text()->kirbytext() ?>
+
+            </div><!-- .o-retain -->
+
+        </div><!-- .o-layer -->
+
+        <?php snippet('contentinfo') ?>
 
     </main>
 
