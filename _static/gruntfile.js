3058cb30451a9ed64142d9e11e5c37c5848b7dd4
Bram Smulders <bram@bramsmulders.com>
2016-10-17 00:04:19 +0200

    incorporated sass in grunt file

--- _static/gruntfile.js
+++ _static/gruntfile.js
@@ -12,6 +12,80 @@ module.exports = function (grunt) {
 
         // This is where our tasks are defined and configured
 
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_source/_sass/**/*.{scss,sass}'],
+                tasks: ['sass:serve']
+            }
+        },
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllPrepare: {
+                command: 'JEKYLL_ENV=production bundle exec jekyll build --config=_config.yml,_config_prod.yml'
+            },
+            jekyllServe: {
+                command: 'JEKYLL_ENV=development bundle exec jekyll serve --config=_config.yml'
+            },
+            deploy: {
+                command: 'firebase deploy'
+            }
+        },
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: false,
+                includePaths: ['_source/assets/bower_components']
+            },
+            serve: {
+                options: {
+                    outputStyle: 'expanded',
+                },
+                files: {
+                    '_build/assets/css/main.css': '_source/_sass/main.scss',
+                    '_source/_includes/critical.css': '_source/_sass/critical.scss'
+                }
+            },
+            build: {
+                options: {
+                    outputStyle: 'compressed',
+                },
+                files: {
+                    '_build/assets/css/main.css': '_source/_sass/main.scss',
+                    '_source/_includes/critical.css': '_source/_sass/critical.scss'
+                }
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
     });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'sass:serve',
+        'concurrent:serve'
+    ]);
+
+    // Register the grunt build task
+    grunt.registerTask('prepare', [
+        'sass:build',
+        'shell:jekyllPrepare',
+        'shell:deploy',
+        //'sass'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'prepare');
 
 };
