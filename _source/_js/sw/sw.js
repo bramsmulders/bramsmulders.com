cd58dfe869c940c1f160c25a3e696087f181642a
Bram Smulders <bram@bramsmulders.com>
2018-03-19 22:26:18 +0100

    Bumped version

--- _source/_js/sw/sw.js
+++ _source/_js/sw/sw.js
@@ -6,7 +6,7 @@
 /*  Variables
 \*----------------------------------------------------------------------------*/
 
-let cacheName = 'bramsmulders:5.1.0';
+let cacheName = 'bramsmulders:5.2.0';
 let cacheFiles = [
   '/about/',
   '/offline/',
@@ -59,7 +59,7 @@ self.addEventListener('fetch', event => {
 \*----------------------------------------------------------------------------*/
 
 self.addEventListener('activate', event => {
-  let cacheWhitelist = ['bramsmulders:5.1.0'];
+  let cacheWhitelist = ['bramsmulders:5.2.0'];
 
   event.waitUntil(
     caches.keys().then(cacheNames => {
