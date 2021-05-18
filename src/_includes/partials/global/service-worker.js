935681571b286a8b53db2e3f151ce21fc0955cbf
Bram Smulders <source@bram.is>
2021-05-18 14:25:03 +0200

    Updated & optimised service worker

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -1,29 +1,28 @@
 const CACHE_KEYS = {
   PRE_CACHE: `precache-${VERSION}`,
-  RUNTIME: `runtime-${VERSION}`
+  RUNTIME: `runtime-${VERSION}`,
 };
 
 // URLS that we don’t want to end up in the cache
 const EXCLUDED_URLS = [
-  'admin',
-  '.netlify',
-  'https://identity.netlify.com/v1/netlify-identity-widget.js',
-  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js',
-  '/reachable',
-  '/thanking-you'
+  "admin",
+  ".netlify",
+  "https://identity.netlify.com/v1/netlify-identity-widget.js",
+  "https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js",
+  "/thanking-you",
 ];
 
 // URLS that we want to be cached when the worker is installed
 const PRE_CACHE_URLS = [
-  '/',
-  '/fonts/ubuntu-v14-latin-700.woff2',
-  '/fonts/crimson-text-v10-latin-regular.woff2',
-  '/fonts/crimson-text-v10-latin-italic.woff2',
-  '/fonts/crimson-text-v10-latin-700.woff2',
+  "/",
+  "/fonts/ubuntu-v14-latin-700.woff2",
+  "/fonts/crimson-text-v10-latin-regular.woff2",
+  "/fonts/crimson-text-v10-latin-italic.woff2",
+  "/fonts/crimson-text-v10-latin-700.woff2",
 ];
 
 // You might want to bypass a certain host
-const IGNORED_HOSTS = ['localhost', 'unpkg.com', ];
+const IGNORED_HOSTS = ["localhost", "unpkg.com"];
 
 /**
  * Takes an array of strings and puts them in a named cache store
@@ -31,27 +30,29 @@ const IGNORED_HOSTS = ['localhost', 'unpkg.com', ];
  * @param {String} cacheName
  * @param {Array} items=[]
  */
-const addItemsToCache = function(cacheName, items = []) {
-  caches.open(cacheName).then(cache => cache.addAll(items));
+const addItemsToCache = function (cacheName, items = []) {
+  caches.open(cacheName).then((cache) => cache.addAll(items));
 };
 
-self.addEventListener('install', evt => {
+self.addEventListener("install", (evt) => {
   self.skipWaiting();
 
   addItemsToCache(CACHE_KEYS.PRE_CACHE, PRE_CACHE_URLS);
 });
 
-self.addEventListener('activate', evt => {
+self.addEventListener("activate", (evt) => {
   // Look for any old caches that don't match our set and clear them out
   evt.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(item => !Object.values(CACHE_KEYS).includes(item));
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (item) => !Object.values(CACHE_KEYS).includes(item)
+        );
       })
-      .then(itemsToDelete => {
+      .then((itemsToDelete) => {
         return Promise.all(
-          itemsToDelete.map(item => {
+          itemsToDelete.map((item) => {
             return caches.delete(item);
           })
         );
@@ -60,8 +61,8 @@ self.addEventListener('activate', evt => {
   );
 });
 
-self.addEventListener('fetch', evt => {
-  const {hostname} = new URL(evt.request.url);
+self.addEventListener("fetch", (evt) => {
+  const { hostname } = new URL(evt.request.url);
 
   // Check we don't want to ignore this host
   if (IGNORED_HOSTS.indexOf(hostname) >= 0) {
@@ -69,27 +70,27 @@ self.addEventListener('fetch', evt => {
   }
 
   // Check we don't want to ignore this URL
-  if (EXCLUDED_URLS.some(page => evt.request.url.indexOf(page) > -1)) {
+  if (EXCLUDED_URLS.some((page) => evt.request.url.indexOf(page) > -1)) {
     return;
   }
 
   evt.respondWith(
-    caches.match(evt.request).then(cachedResponse => {
+    caches.match(evt.request).then((cachedResponse) => {
       // Item found in cache so return
       if (cachedResponse) {
         return cachedResponse;
       }
 
       // Nothing found so load up the request from the network
-      return caches.open(CACHE_KEYS.RUNTIME).then(cache => {
+      return caches.open(CACHE_KEYS.RUNTIME).then((cache) => {
         return fetch(evt.request)
-          .then(response => {
+          .then((response) => {
             // Put the new response in cache and return it
             return cache.put(evt.request, response.clone()).then(() => {
               return response;
             });
           })
-          .catch(ex => {
+          .catch((ex) => {
             return;
           });
       });
