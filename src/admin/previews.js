6270a60fd15b681a1ed5c1ae118e728a6373da1d
Bram Smulders <source@bram.is>
2021-06-04 12:07:36 +0200

    remove show theme credit

--- src/admin/previews.js
+++ src/admin/previews.js
@@ -1,20 +1,15 @@
-const {
-  w3DateFilter,
-  markdownFilter,
-  dateFilter,
-  helpers,
-} = previewUtil;
+const { w3DateFilter, markdownFilter, dateFilter, helpers } = previewUtil;
 
 const env = nunjucks.configure();
 
-env.addFilter('w3DateFilter', w3DateFilter);
-env.addFilter('markdownFilter', markdownFilter);
-env.addFilter('dateFilter', dateFilter);
+env.addFilter("w3DateFilter", w3DateFilter);
+env.addFilter("markdownFilter", markdownFilter);
+env.addFilter("dateFilter", dateFilter);
 
 const Preview = ({ entry, path, context }) => {
-  const data = context(entry.get('data').toJS());
+  const data = context(entry.get("data").toJS());
   const html = env.render(path, { ...data, helpers });
-  return <div dangerouslySetInnerHTML={{ __html: html }}/>
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 const Home = ({ entry }) => (
@@ -27,13 +22,15 @@ const Home = ({ entry }) => (
       postsHeading,
       archiveButtonText,
       collections: {
-        postFeed: [{
-          url: 'javascript:void(0)',
-          date: new Date(),
-          data: {
-            title: 'Sample Post',
+        postFeed: [
+          {
+            url: "javascript:void(0)",
+            date: new Date(),
+            data: {
+              title: "Sample Post",
+            },
           },
-        }],
+        ],
       },
     })}
   />
@@ -46,7 +43,7 @@ const Post = ({ entry }) => (
     context={({ title, date, body }) => ({
       title,
       date,
-      content: markdownFilter(body || ''),
+      content: markdownFilter(body || ""),
     })}
   />
 );
@@ -57,7 +54,7 @@ const Page = ({ entry }) => (
     path="layouts/page.njk"
     context={({ title, body }) => ({
       title,
-      content: markdownFilter(body || ''),
+      content: markdownFilter(body || ""),
     })}
   />
 );
@@ -66,11 +63,10 @@ const SiteData = ({ entry }) => (
   <Preview
     entry={entry}
     path="layouts/base.njk"
-    context={({ name, shortDesc, showThemeCredit }) => ({
+    context={({ name, shortDesc }) => ({
       site: {
         name,
         shortDesc,
-        showThemeCredit,
       },
     })}
   />
@@ -88,8 +84,8 @@ const Nav = ({ entry }) => (
   />
 );
 
-CMS.registerPreviewTemplate('home', Home);
-CMS.registerPreviewTemplate('posts', Post);
-CMS.registerPreviewTemplate('generic_pages', Page);
-CMS.registerPreviewTemplate('site_data', SiteData);
-CMS.registerPreviewTemplate('nav', Nav);
+CMS.registerPreviewTemplate("home", Home);
+CMS.registerPreviewTemplate("posts", Post);
+CMS.registerPreviewTemplate("generic_pages", Page);
+CMS.registerPreviewTemplate("site_data", SiteData);
+CMS.registerPreviewTemplate("nav", Nav);
